3bf7fde04f9676d17a9f56eb76d0e2354139bc9f
Tim D. Smith <tdsmith@mozilla.com>
2020-09-13 10:51:50 -0700

    Don't assume experimets have segments

--- invoke.py
+++ invoke.py
@@ -201,6 +201,8 @@ class Result:
 
     @property
     def segments(self) -> List[str]:
+        if "segments" not in self.data.columns:
+            return ["all"]
         segments = set(self.data.segment.fillna("all").drop_duplicates())
         segments.remove("all")
         l = ["all"] + sorted(segments)
